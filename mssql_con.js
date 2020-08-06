cae7309d644ff11e793c2aa238ac390700498d4b
David Ybanez <dbybanez@gmail.com>
2020-08-06 15:48:08 +0800

    added mssql support

--- mssql_con.js
+++ mssql_con.js
@@ -1,42 +1,80 @@
 'use strict';
-console.log('Test DB connection for MSSQL or SQL Server');
+console.log('Test DB connection for MSSQL (SQL Server)');
 const mssql = require('mssql')
 
 async function main() {
   const config = {
-    user: '',
-    password: '',
-    server: 'localhost',
-    database: 'retaildb_mssql'
+    user: 'admin',
+    password: 'admin',
+    server: 'DAVID-LAPTOP\\SQLEXPRESS01',
+    database: 'retaildb_mssql',
+    port: 1433,
+    options: {
+      enableArithAbort: false
+    }
   }
 
-  try {
-    var connection = new mssql.Connection(config)
-    //let pool = await mssql.connect(config)
+  let pool = null
 
-    employeelist = await getEmployeeList(connection)
+  try {
+    pool = await mssql.connect(config)
+    console.log('Connection successful')
+
+    let tablelist = await listTables(pool)
+    console.log("Tables:")
+    tablelist.forEach(function (elem, index) {
+      console.log(` - ${elem.TABLE_NAME}`)
+    })
+    
+    let employeelist = await getEmployeeList(pool)
     console.log("Employee List:")
     console.log(employeelist)
+
+    employeelist.forEach(function (elem, index) {
+      console.log(elem.EmployeeID + " | " + elem.FirstName + " | " + elem.LastName)
+    })
 
 
   } catch (e) {
     console.error(e)
   } finally {
-    
+    pool.close()
   }
 }
 
-async function getEmployeeList(connection) {
+async function listTables(pool) {
+  return new Promise((resolve, reject) => {
+    var result
+    var query_result = []
+    var res
+    let query = "SELECT Distinct TABLE_NAME FROM INFORMATION_SCHEMA.TABLES"
+    pool.request().query(query, function (error, results, fields) {
+      if (error) throw error
+      // console.log(results.recordset)
+      if (results.recordset.length > 0) {
+        for (result in results.recordset) {
+          // console.log(results.recordset[result].TABLE_NAME)
+          query_result.push(results.recordset[result])
+        }
+      }
+      res = JSON.parse(JSON.stringify(query_result))
+      resolve(res)
+    })
+  })
+}
+
+async function getEmployeeList(pool) {
   return new Promise((resolve, reject)  => {
     var result
     var query_result = []
     var res
-    var query = "SELECT * FROM employeelist"
-    connection.query(query, function (error, results, fields) {
+    var query = "SELECT * FROM EmployeeList"
+    pool.request().query(query, function (error, results, fields) {
       if (error) throw error
-      if(results.length > 0) {
-        for (result in results) {
-          query_result.push(results[result])
+      // console.log(results.recordset)
+      if(results.recordset.length > 0) {
+        for (result in results.recordset) {
+          query_result.push(results.recordset[result])
         }
       }
       res = JSON.parse(JSON.stringify(query_result))
